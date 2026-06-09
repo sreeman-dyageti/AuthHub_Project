3dcf904d72833cbe866ba9d2e2c535cb2a908c85
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-06-10 03:01:18 +0530

    feat: add user registration controller

---
+++ src/modules/auth/auth.controller.js
@@ -0,0 +1,25 @@
+import { registerUser } from './auth.service.js';
+
+export const register = async (req, res) => {
+  try {
+    
+    const { email, password, first_name, last_name } = req.body;
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Email and password are required.' });
+    }
+    
+    const result = await registerUser({ email, password, first_name, last_name });
+    // send successfully message
+    res.status(201).json({
+      message: 'User registered successfully. Please verify your email.',
+      data: {
+        user: result.user,
+        verificationToken: result.verificationToken
+      }
+    });
+
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
