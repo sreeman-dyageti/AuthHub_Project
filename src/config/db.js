07cd657ceab95b9512f44793863047254ce98910
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-06-09 21:23:38 +0530

    feat: Changed the Db connction from local to Neon Db

--- src/config/db.js
+++ src/config/db.js
@@ -1,27 +1,15 @@
 import pg from 'pg';
 import dotenv from 'dotenv';
 
-// Load environment variables
-dotenv.config({ path: "../.env" });
+dotenv.config({path: "./.env"});
 
 const { Pool } = pg;
 
-console.log({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_NAME,
-  port: process.env.PG_PORT,
-});
-
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_NAME,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false 
+  }
 });
 
-// Exporting the query function
-export function query(text, params) {
-  return pool.query(text, params);
-} 
+export const query = (text, params) => pool.query(text, params);
