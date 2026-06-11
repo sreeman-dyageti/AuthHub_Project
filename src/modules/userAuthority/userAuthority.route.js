da7bb7958cdfee37cd6af46c7db86a5c66db720e
DINESH656 <dineshkumar135514@gmail.com>
2026-06-11 21:23:14 +0530

    connected the router to controller   for the verification of user authority

--- src/modules/userAuthority/userAuthority.route.js
+++ src/modules/userAuthority/userAuthority.route.js
@@ -1,5 +1,7 @@
 import express from 'express';
-import { inviteUser } from './userAuthority.controller.js';
+import { verifyUser , inviteUser } from './userAuthority.controller.js';
+
 const router = express.Router();
 router.post('/invite', inviteUser);
+router.post('/verify', verifyUser );
 export default router;
