947e288e27ce2c3767ec537232328a3a9062fa50
Dyageti.Sai Sreeman <260058045+sreeman-dyageti@users.noreply.github.com>
2026-06-13 00:42:06 +0530

    feat: implement authentication, authorization middleware and role-based access control

---
+++ src/middleware/authorization.middleware.js
@@ -0,0 +1,15 @@
+export const authorize = (allowedRoles) => {
+
+  return (req, res, next) => {
+
+    if (!allowedRoles.includes(req.user.role)) {
+
+      return res.status(403).json({
+        error: 'Access denied'
+      });
+
+    }
+    next();
+  };
+
+};
--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { register, login, verifyEmail } from "./auth.controller.js";
+import { authentication} from "../../middleware/authentication.middleware.js";
+import {authorize} from "../../middleware/authorization.middleware.js"
 
 // Registration router endpoint
 const router = express.Router();
@@ -11,6 +13,21 @@ router.get('/verify-email', verifyEmail);
 
 // Login router endpoint
 router.post('/login',login);
+
+// authentication endpoint to verify the JWT
+router.get('/profile', authentication,(req, res) => {
+    res.json({
+      message: 'Protected route accessed',
+      user: req.user
+    });
+  });
+
+// protected routes 
+router.get('/admin', authentication, authorize(["admin"]), (req, res) => {
+    res.json({
+      message: 'Welcome Admin'
+    });
+  });
 
 export default router;
 
